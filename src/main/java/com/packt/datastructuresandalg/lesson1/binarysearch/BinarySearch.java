ab7f2526722c64fd3e5593f5e4f2d67b80ec00ca
radekczdev <radekcz.dev@gmail.com>
2024-08-12 22:29:50 +0200

    Converted to recursive BinarySearch

--- src/main/java/com/packt/datastructuresandalg/lesson1/binarysearch/BinarySearch.java
+++ src/main/java/com/packt/datastructuresandalg/lesson1/binarysearch/BinarySearch.java
@@ -1,43 +1,48 @@
 package com.packt.datastructuresandalg.lesson1.binarysearch;
 
 public class BinarySearch {
-    public boolean binarySearch(int x, int[] sortedNumbers) {
-        System.out.println("Looking for: " + x);
-        boolean containsX = false;
-        int start = 0;
-        int end = sortedNumbers.length - 1;
+  public static void main(String[] args) {
+    BinarySearch binarySearch = new BinarySearch();
+    System.out.println(binarySearch.binarySearch(7, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+    System.out.println(binarySearch.binarySearch(0, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
+    System.out.println(binarySearch.binarySearch(1, new int[] {1, 2, 3, 4, 5}));
+  }
 
-        while(!containsX && start <= end) {
-            int mid = (end - start)/2 + start;
-            final var valueAtMid = sortedNumbers[mid];
-            printArrayWithHighlights(sortedNumbers,start,mid,end);
-            if(valueAtMid == x) {
-                containsX = true;
-            } else if(valueAtMid > x) {
-                end = mid - 1;
-            } else {
-                start = mid + 1;
-            }
-        }
+  public boolean binarySearch(int x, int[] sortedNumbers, int start, int end) {
+    System.out.println("Looking for: " + x);
+    if (end > start) {
+      int mid = (end - start) / 2 + start;
+      printArrayWithHighlights(sortedNumbers, start, mid, end);
 
-        return containsX;
-
+      final var valueAtMid = sortedNumbers[mid];
+      if (valueAtMid == x) {
+        return true;
+      } else if (valueAtMid > x) {
+        return binarySearch(x, sortedNumbers, start, mid - 1);
+      } else {
+        return binarySearch(x, sortedNumbers, mid + 1, end);
+      }
     }
+    return false;
+  }
 
-    private void printArrayWithHighlights(int[] array, int start, int mid, int end) {
-        for (int i = 0; i < array.length; i++) {
-            if (i == start) System.out.print("[S]");
-            if (i == mid) System.out.print("[M]");
-            if (i == end) System.out.print("[E]");
-            System.out.print(array[i] + " ");
-        }
-        System.out.println();
-    }
+  public boolean binarySearch(int x, int[] sortedNumbers) {
+    return binarySearch(x, sortedNumbers, 0, sortedNumbers.length - 1);
+  }
 
-    public static void main(String args[]) {
-        BinarySearch binarySearch = new BinarySearch();
-        System.out.println(binarySearch.binarySearch(7, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
-        System.out.println(binarySearch.binarySearch(0, new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}));
-        System.out.println(binarySearch.binarySearch(1, new int[]{1, 2, 3, 4,5}));
+  private void printArrayWithHighlights(int[] array, int start, int mid, int end) {
+    for (int i = 0; i < array.length; i++) {
+      if (i == start) {
+        System.out.print("[S]");
+      }
+      if (i == mid) {
+        System.out.print("[M]");
+      }
+      if (i == end) {
+        System.out.print("[E]");
+      }
+      System.out.print(array[i] + " ");
     }
+    System.out.println();
+  }
 }
